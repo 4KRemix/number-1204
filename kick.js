00f0a18c855df7ccb149d428fe602c12e532bdf6
4KRemix <38147866+4KRemix@users.noreply.github.com>
2018-04-09 17:16:44 -0600

    Delete kick.js

--- kick.js
+++
@@ -1,8 +0,0 @@
-function kickUser(username){
-    var users = turntable.topViewController.users;
-    for (var userId in users){
-        if (users[userId].name == username){
-            ROOMMANAGER.callback('boot_user',userId);
-        }
-    }
-};
